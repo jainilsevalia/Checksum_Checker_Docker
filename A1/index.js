b58a9fcf18442819cb1071c9edd9ddaf3e17d809
Jainil Sevalia <jn498899@dal.ca>
2023-02-05 10:06:55 -0400

    Docker initial

---
+++ A1/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios')
+const fs = require('fs');
+const app = express();
+const PORT = 5000;
+
+const path = '../Shared-volume/'
+
+app.use(express.json())
+app.listen(PORT,()=>{console.log(`it's Working`)})
+
+app.get('/checksum',(req,res)=>{
+    res.status(200).send({
+        file:'You have to submit File name through POST request in JSON Formate.'
+    })
+});
+
+app.post('/checksum',(req,res)=>{
+    const {file} = req.body;
+    if(!file){
+        res.status(403).send({file:null,error:'Invalid JSON input.'});
+    }
+        if(fs.existsSync(`../${file}`)){
+            axios.post('http://server:8000/checksum-cal', {
+                file:`${file}`,
+            })
+            .then(function (response) {
+                console.log(response);
+                res.status(200).send({file:response.data.file,checksum:response.data.checksum});
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+            return;
+        }
+        else{
+        res.status(404).send({file:`${file}`,error:'File not found.'})
+    }
+});
---
+++ A1_P2/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const crypto = require('crypto');
+const fs = require('fs');
+const app = express();
+const PORT =  8000;
+
+app.use(express.json())
+app.listen(PORT)
+
+app.post('/checksum-cal',(req,res)=>{
+    const {file} = req.body;
+    fs.readFile(`../${file}`,'utf8',(err,data)=>{
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            var checksum = crypto.createHash('md5').update(data).digest('hex');
+            console.log(checksum);
+            res.status(200).send({file:`${file}`,checksum:checksum})
+        }
+    })
+})
